b6c2aa2fa97a63c06ddeb6a5a38c5a6684977148
Dcsk <96806975+Saravana200@users.noreply.github.com>
2022-03-06 17:35:35 +0000

    p5-p6

--- p5final.c
+++ p5final.c
@@ -1,47 +1,38 @@
 #include <stdio.h>
-int input()
+int input_size()
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+    int a;
+    printf("enter the size of the array\n");
+    scanf("%d",&a);
+    return a;
 }
-
-void init_array(int n, int a[n])
+void input_array(int n,int a[n])
 {
-  for(int i=0;i<n;i++)
-    a[i] = i;
-  a[1]=0;
+    printf("enter the array\n");
+    for(int i=0;i<n;i++)
+     scanf("%d",&a[i]);
 }
-
-void ets(int n, int a[n])
+int find_largest(int n,int a[n])
 {
-  int i=0;
-   /* Find next non-zero number */
-  while(i<sqrt(n)){
-   for(;a[i]==0 ;i++);
-   for(int k=i+i;k<n;k += i)
-     a[i] = 0;
-   i++;
-  }
+    int l=a[0];
+    for(int i=1;i<n;i++)
+    {  
+      if(l<a[i])
+        l=a[i];
+    }
+    return l;
 }
-
-
-void display(int n, int a[n])
+void out_put(int n,int a[n],int largest)
 {
-  for(int i=0;i<n;i++)
-    if(a[i]!=0)
-      printf("%d ",a[i]);
-  printf("\n");
+    printf("the largest among ");
+    for(int i=0;i<n-1;i++)
+        printf("%d,",a[i]);
+    printf("%d is %d",a[n-1],largest);
 }
-
 int main()
 {
-  int n;
-  n=input();
-  int a[n];
-  init_array(n,a);
-  ets(n,a);
-  display(n,a);
-  return 0;
+    int n=input_size();
+    int a[n];
+    input_array(n,a);
+    out_put(n,a,find_largest(n,a));
 }
--- p6final.c
+++ p6final.c
@@ -1,32 +1,30 @@
-/* Write a program to find the index of a substring of a string.
-void input_string(char *a);
-int str_reverse(char *string, char *substring);
-void output(char *string, char *substring, int index); */
-
-int string_length(char *s)
+#include <stdio.h>
+#include <string.h>
+void input_string(char *a)
 {
-  int i;
-  for(i=0;s[i]!='\0';i++);
-  return i;
+    printf("enter the string\n");
+    fgets(a,20,stdin);
 }
-
-int string_ncmp(char *s1,char *s2)
+int count_words(char *string)
 {
-  for(int i=0; i<n && s1[i]==s2[i] && s1[i] != '\0';i++);
-  return s1[i] - s2[i];
+    char *token=strtok(string," ");
+    int count=0;
+    while(token!=NULL)
+    {
+        count++;
+        printf("%s ",token);
+        token=strtok(NULL," ");
+    }
+    return count;
 }
-
-
-int string_index(char *s, char *subs)
+void output(char *string,int no_words)
 {
-  int l1=string_lenght(s);
-  int l2=string_lenght(subs);
-  if l1 < l2
-      return -1;
-  for(int i=0;i<l1-l2;i++)
-    if (string_nscmp(s,subs))
-      return i;
-  return -1;
+    printf("is %d",no_words);
 }
-
-/* Understand and debug the functions and write main */
+int main()
+{
+    char s[20];
+    input_string(s);
+    printf("the number of words in the string ");
+    output(s,count_words(s));
+}
